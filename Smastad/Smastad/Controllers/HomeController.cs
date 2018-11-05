e913c561cffe18a2ba730d5d5b0d7c3eb71d0e10
Gugu2018 <limolback@hotmail.se>
2018-11-05 16:23:32 +0100

    [task] add Models folder

--- Smastad/Smastad/Controllers/HomeController.cs
+++ Smastad/Smastad/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using Smastad.Models;
+
 namespace Smastad.Controllers
 {
   public class HomeController : Controller
---
+++ Smastad/Smastad/Models/Errand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smastad.Models
+{
+    public class Errand
+    {
+      public string ErrandId { get; set; }
+    }
+}
