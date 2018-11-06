d59fa81d0b5dec0a79b95926ecf00114ba0c3aa0
Gugu2018 <limolback@hotmail.se>
2018-11-06 16:36:47 +0100

    [task] add cookies till main sida & login sida

--- Smastad/Smastad/Controllers/HomeController.cs
+++ Smastad/Smastad/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net;
 
 using Smastad.Models;
 
@@ -41,6 +41,45 @@ namespace Smastad.Controllers
         return View();
       }
       
+    }
+
+    //skapa en cookie
+    public ViewResult WriteCookies(string settting, string settingValue, bool isPersistent)
+    {
+      if (isPersistent)
+      {
+        CookieOptions options = new CookieOptions();
+        options.Expires = DateTime.Now.AddDays(1);
+        Response.Cookies.Append(settting, WebUtility.UrlEncode(settingValue), options);
+      }
+      else
+      {
+        Response.Cookies.Append(settting, WebUtility.UrlEncode(settingValue));
+      }
+      ViewBag.Message = "Cookie written successfully!";
+      return View("Index");
+    }
+
+    //Läsa data från cookien
+    public ViewResult ReadCookies()
+    {
+      ViewBag.FontName = WebUtility.UrlDecode(Request.Cookies["fontName"]);
+      ViewBag.FontSize = WebUtility.UrlDecode(Request.Cookies["fontSize"]);
+      ViewBag.Color = WebUtility.UrlDecode(Request.Cookies["color"]);
+
+      if (string.IsNullOrEmpty(ViewBag.FontName))
+      {
+        ViewBag.FontName = "Arial"; //text visas när man klickar Read Cookies
+      }
+      if (string.IsNullOrEmpty(ViewBag.FontSize))
+      {
+        ViewBag.FontSize = "22px";
+      }
+      if (string.IsNullOrEmpty(ViewBag.Color))
+      {
+        ViewBag.Color = "Blue";
+      }
+      return View();
     }
 
   }
