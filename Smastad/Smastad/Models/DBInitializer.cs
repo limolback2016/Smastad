4ae969089c2332ed76633ddee1e9795f0ae3ac28
Gugu2018 <limolback@hotmail.se>
2018-11-26 22:03:15 +0100

    Fixed Layouts

--- Smastad/Smastad/Models/DBInitializer.cs
+++ Smastad/Smastad/Models/DBInitializer.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Smastad.Models
 {
-  public class DBInitializer
+    public class DBInitializer
   {
     public static void EnsurePopulated(ApplicationDbContext context)
     {
